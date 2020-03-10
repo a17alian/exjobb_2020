667df1270539a87e5f9500bcc52fe6d357e44614
Alice A <aliceanglesjo@Alices-MacBook-Pro.local>
2020-03-10 13:12:39 +0100

    Print out data
    Now able to print out data from database to list

--- routes/index.js
+++ routes/index.js
@@ -10,26 +10,22 @@ router.get('/floods', function(req, res, next) {
   var MongoClient = mongodb.MongoClient;
   var url = 'mongodb://localhost:27017/test';
 
-  MongoClient.connect(url, function(err, db){
-    if(err){
-      console.log('Unable to connect ', err)
-    } else {
-      console.log('Connected!');
-      var colletion = db.colletion('Floods');
-      colletion.find({}).toArray(function(err, result){
-        if(err){
-          res.send(err);
-        } else if(result.length){
-          res.render('floods', {
-            "floods": result
-          });
-        } else {
-          res.send('No documents found')
-        }
-        db.close();
-
-      });
-    }
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    //Find the first document in the customers collection:
+    dbo.collection("floods").find({}).toArray(function(err, result) {
+      if (err){
+        res.send(err);
+      } else if(result.length){
+        res.render('floods', {
+          "floods" : result
+        });
+      } else {
+        res.send('No documents found');
+      }
+      db.close();
+    });
   });
 });
 module.exports = router;
