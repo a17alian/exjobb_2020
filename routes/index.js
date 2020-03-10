9b75a8dbca665beea4514f77647ed9e697c16008
Alice A <aliceanglesjo@Alices-MacBook-Pro.local>
2020-03-10 12:02:31 +0100

    Mongodb connection
    Connecting mongodb database and jade file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
+var mongodb = require('mongodb');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/floods', function(req, res, next) {
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/test';
+
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log('Unable to connect ', err)
+    } else {
+      console.log('Connected!');
+      var colletion = db.colletion('Floods');
+      colletion.find({}).toArray(function(err, result){
+        if(err){
+          res.send(err);
+        } else if(result.length){
+          res.render('floods', {
+            "floods": result
+          });
+        } else {
+          res.send('No documents found')
+        }
+        db.close();
+
+      });
+    }
+  });
+});
 module.exports = router;
