251ecc29d712138e97272508e0404c5d39d20e7d
Alice A <aliceanglesjo@Alices-MacBook-Pro.local>
2020-03-12 12:01:33 +0100

    Renamed routes
    One for /openlayers and one for /leaflet

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Map App' });
   res.send('index', res);
 });
 
-router.get('/floods', function(req, res, next) {
+router.get('/leaflet', function(req, res, next) {
   var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://localhost:27017/test';
-
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("test");
-    //Find the first document in the customers collection:
-    dbo.collection("floods").find({}).toArray(function(err, result) {
-      if (err){
-        res.send(err);
-      } else if(result.length){
-        res.render('floods', {
-          "floods" : result
-        });
-      } else {
-        res.send('No documents found');
-      }
-      db.close();
+var url = 'mongodb://localhost:27017/test';
+    
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("test");
+      //Find all in the floods collection:
+      dbo.collection("floods").find({}).toArray(function(err, result) {
+        if (err){
+          res.send(err);
+        } else if(result.length){
+          res.render('leaflet', {
+            "floods" : result
+          });
+        } else {
+          res.send('No documents found');
+        }
+        db.close();
+      });
     });
-  });
 });
+
+router.get('/openlayers', function(req, res, next) {
+  var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/test';
+    
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("test");
+      //Find all in the floods collection:
+      dbo.collection("floods").find({}).toArray(function(err, result) {
+        if (err){
+          res.send(err);
+        } else if(result.length){
+          res.render('ol', {
+            "floods" : result
+          });
+        } else {
+          res.send('No documents found');
+        }
+        db.close();
+      });
+    });
+
+});
+
 module.exports = router;
