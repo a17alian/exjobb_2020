f0f5eedd8c842158184792b1e7b8142a3e454b07
Alice A <aliceanglesjo@Alices-MacBook-Pro.local>
2020-03-18 17:50:29 +0100

    Attempting to convert json to GeoJSON
    Must convert to GeoJSON since ol does not seem to take JSON as input

--- public/javascripts/ol.js
+++ public/javascripts/ol.js
@@ -3,50 +3,61 @@ $.ajax({
     type: 'GET',
     dataType: 'json', // added data type
     success: function(res) {
-        generateMarkers(res);
+        toGeoJson(res);
 
     }
 });
 function generateMarkers(floods){
-
+  for(var i = 0; i < 100; i ++){
+    heatmapObj[i] = {lat: floods[i].lat, lng: floods[i].long}
+    coordsData.data.push(heatmapObj[i]);
 }
 
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
-    })
-});
-
-
-var vector = new ol.layer.Heatmap({
-  source: new ol.source.Vector({
-    url: 'data/kml/2012_Earthquakes_Mag5.kml',
-    format: new ol.format.KML({
-      extractStyles: false
-    })
-  }),
-  blur: 10,
-  radius: 10,
-  weight: function(feature) {
-    // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
-    // standards-violating <magnitude> tag in each Placemark.  We extract it from
-    // the Placemark's name instead.
-    var name = feature.get('name');
-    var magnitude = parseFloat(name.substr(2));
-    return magnitude - 5;
+}
+var geojsonObject = {
+  type: 'FeatureCollection',
+  features: []
+};
+var marker = {
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: []
   }
+};
+function toGeoJson(floods) {
+
+
+  for(var i = 0; i < 100; i ++){
+    marker.coordinates =  floods[i].lat , floods[i].long;
+
+    geojsonObject.features.push(marker[i]);
+  }
+  console.log( geojsonObject.features);
+  return geojsonObject;
+};
+console.log(geojsonObject);
+// Create a heatmap layer based on GeoJSON content
+var heatmapLayer = new ol.layer.Heatmap({
+  source: new ol.source.Vector({
+    features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
+  }),
+  opacity: 0.9
 });
 
-var raster = new ol.layer.Tile({
-  source: new ol.source.Stamen({
-    layer: 'toner'
+// Create a tile layer from OSM
+var osmLayer = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+// Create the map with the previous layers
+var map = new ol.Map({
+  target: 'map',  // The DOM element that will contains the map
+  renderer: 'canvas', // Force the renderer to be used
+  layers: [osmLayer, heatmapLayer],
+  // Create a view centered on the specified location and zoom level
+  view: new ol.View({
+      center: ol.proj.transform([2.1833, 41.3833], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 4
   })
 });
-
