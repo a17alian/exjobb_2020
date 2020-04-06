007a63325c4b9a620fd04996e66c7a85d3457ca8
a17alian <>
2020-04-06 09:28:24 +0200

    OL: Custom marker test working

--- public/javascripts/ol.js
+++ public/javascripts/ol.js
@@ -31,7 +31,7 @@ function toGeoJson(floods) {
 };
 
 var iconFeature = new ol.Feature({
-  geometry: new ol.geom.Point([0, 0]),
+  geometry: new ol.geom.Point(ol.proj.fromLonLat([15.037866699999995, 59.51920639999999])),
   name: 'Null Island',
   population: 4000,
   rainfall: 500
@@ -42,37 +42,38 @@ var iconStyle = new ol.style.Style({
     anchor: [0.5, 46],
     anchorXUnits: 'fraction',
     anchorYUnits: 'pixels',
-    src: 'data/icon.png'
+    src: '../avo.png'
   })
 });
 setTimeout(function(){
+  // Heatmap
   var vectorSource =  new ol.source.Vector({
     features: new ol.format.GeoJSON().readFeatures(geojsonObject)
   });
-   vector = new ol.layer.Heatmap({
+  var vector = new ol.layer.Heatmap({
     source: vectorSource,
     radius: 12,
     opacity: 0.6,
 
   });
-  
-  iconFeature.setStyle(iconStyle);
-
+  // Marker
   var markerSource = new ol.source.Vector({
     features: [iconFeature]
   });
-  
+
   var markerLayer = new ol.layer.Vector({
     source: markerSource
   });
-  
+ 
+  iconFeature.setStyle(iconStyle);
+
 
   var raster = new ol.layer.Tile({
     source: new ol.source.OSM()
   });
   
   var map = new ol.Map({
-    layers: [raster, markerLayer],
+    layers: [raster , markerLayer],
     target: 'map',
     view: new ol.View({
       center: ol.proj.fromLonLat([13.404954, 52.520008]),
@@ -89,19 +90,42 @@ setTimeout(function(){
    offset: [0, -50]
  });
  map.addOverlay(popup);
-   
- }, 200);
+
+ 
 
  $( "#controlls" ).click(function() {
-  switch($('input[type=radio][name=layers]:checked').attr('id')){
+  var clicked_id = $('input[type=radio][name=layers]:checked').attr('id');
+  switch(clicked_id ){
    case 'heatmap':
-     map.setLayerGroup(vector);
+       console.log(clicked_id);
      break;
    case 'markers':
+       console.log(clicked_id);
      break; 
-     map.setLayerGroup(layersMQ);
    default: 
      console.log('Not a id');
   }
-
 });
+
+// display popup on click
+map.on('click', function(evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+    function(feature) {
+      return feature;
+    });
+  if (feature) {
+    var coordinates = feature.getGeometry().getCoordinates();
+    popup.setPosition(coordinates);
+    $(element).popover({
+      placement: 'top',
+      html: true,
+      content: feature.get('name')
+    });
+    $(element).popover('show');
+  } else {
+    $(element).popover('destroy');
+  }
+});
+
+
+ }, 200);
