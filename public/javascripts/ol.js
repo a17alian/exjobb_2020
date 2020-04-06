c18c14afccea0cc9d2dd82d11295fc528a10119b
a17alian <>
2020-04-06 17:08:07 +0200

    OL: Multiple  icons is workning
    Having errors with popup not showing correct info when clicking icon

--- public/javascripts/ol.js
+++ public/javascripts/ol.js
@@ -1,11 +1,10 @@
-
 $.ajax({
-    url: "http://localhost:3000/data",
-    type: 'GET',
-    dataType: 'json', // added data type
-    success: function(res) {
-      toGeoJson(res);
-    }
+  url: "http://localhost:3000/data",
+  type: 'GET',
+  dataType: 'json', // added data type
+  success: function(res) {
+    generateMarkers(res);
+  }
 });
 
 var geojsonObject = {
@@ -30,46 +29,47 @@ function toGeoJson(floods) {
   return geojsonObject
 };
 
-var iconFeature = new ol.Feature({
-  geometry: new ol.geom.Point(ol.proj.fromLonLat([15.037866699999995, 59.51920639999999])),
-  name: 'Null Island',
-  population: 4000,
-  rainfall: 500
-});
 
 var iconStyle = new ol.style.Style({
   image: new ol.style.Icon({
     anchor: [0.5, 46],
     anchorXUnits: 'fraction',
     anchorYUnits: 'pixels',
+    scale: 0.15,
     src: '../avo.png'
   })
 });
-setTimeout(function(){
+var iconFeatures = [];
+
+function generateMarkers(floods) {
+  for(var i = 0; i < 100; i ++){
+    var iconFeature = new ol.Feature({geometry: new ol.geom.Point(ol.proj.fromLonLat([floods[i].long, floods[i].lat])), country: floods[i].country, cause: floods[i].maincause, dead: floods[i].dead, began: floods[i].began, ended: floods[i].ended
+    });
+    iconFeature.setStyle(iconStyle);
+    markerSource.addFeature(iconFeature);
+  } // for loop end
+}
   // Heatmap
-  var vectorSource =  new ol.source.Vector({
+  var heatmapSource =  new ol.source.Vector({
     features: new ol.format.GeoJSON().readFeatures(geojsonObject)
   });
-  var vector = new ol.layer.Heatmap({
-    source: vectorSource,
+  var heatmapLayer = new ol.layer.Heatmap({
+    source: heatmapSource,
     radius: 12,
     opacity: 0.6,
 
   });
+
   // Marker
-  var markerSource = new ol.source.Vector({
-    features: [iconFeature]
-  });
+  var markerSource = new ol.source.Vector({});
 
   var markerLayer = new ol.layer.Vector({
     source: markerSource
   });
- 
-  iconFeature.setStyle(iconStyle);
-
 
   var raster = new ol.layer.Tile({
-    source: new ol.source.OSM()
+    source: new ol.source.OSM(),
+    crossOrigin: ''
   });
   
   var map = new ol.Map({
@@ -87,11 +87,9 @@ setTimeout(function(){
    element: element,
    positioning: 'bottom-center',
    stopEvent: false,
-   offset: [0, -50]
+   offset: [0, -10]
  });
- map.addOverlay(popup);
-
- 
+ map.addOverlay(popup)
 
  $( "#controlls" ).click(function() {
   var clicked_id = $('input[type=radio][name=layers]:checked').attr('id');
@@ -119,7 +117,7 @@ map.on('click', function(evt) {
     $(element).popover({
       placement: 'top',
       html: true,
-      content: feature.get('name')
+      content: '<span> Country: ' + feature.get('country') + '</span>', 
     });
     $(element).popover('show');
   } else {
@@ -128,4 +126,3 @@ map.on('click', function(evt) {
 });
 
 
- }, 200);
